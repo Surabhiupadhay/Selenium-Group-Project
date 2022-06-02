d9a19dd53134ca553b3ce67f34d616781f7aecf7
Surabhi Upadhay <106726111+Surabhiupadhay@users.noreply.github.com>
2022-06-02 15:05:15 -0500

    Add files via upload

---
+++ Firstloopassignment.java
@@ -0,0 +1,15 @@
+package nest_loopassignment;
+
+public class Firstloopassignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int row=5;
+     for (int i=1; i <=row; i--) {
+    	 for (int j= 1; j<=i; j--)
+    		 System.out.println(j+"/");
+    	 
+	}
+System.out.println("");
+}
+}
